e4cf79d861635a42850f9903d56afc68f85a4642
Colonna45 <ncolonna45@gmail.com>
2025-12-07 14:27:21 -0600

    Create voice.js

---
+++ api/voice.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/xml ");
+  res.send(`
+    <Response>
+      < Say >Hello Nick, Iron-Core AI is online.< /Say >
+    < /Response >
+  `);
+}
