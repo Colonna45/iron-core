5c93870fc3fa75e47581f058ec29fd9d38a07d4d
Colonna45 <ncolonna45@gmail.com>
2025-12-07 14:46:42 -0600

    Update voice.js

--- api/voice.js
+++ api/voice.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   res.setHeader("Content-Type", "text/xml");
   res.send(`
     <Response>
-      < Say >Hello Nick, Iron-Core AI is online.< /Say >
+      <Say>Hello Nick, Iron-Core AI is online.</Say>
     </Response>
   `);
 }
